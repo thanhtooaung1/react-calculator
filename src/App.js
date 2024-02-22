7c86580035968f95c9a12c801c8fc0c975a27fcd
thanhtooaung1 <thanhtooaung@gicjp.com>
2024-02-22 11:02:30 +0630

    react calculator finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number1, setNumber1] = useState();
+  const [number2, setNumber2] = useState();
+  const [result, setResult] = useState();
+  const [error, setError] = useState();
+
+  const addValues = (e) => {
+    e.preventDefault();
+    const result = number1 + number2;
+    if (!isNaN(result)) {
+      setError();
+      setResult(number1 + number2);
+    } else {
+      setError("Invalid values");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <form>
+          <h3 className="text">Sum Calculator</h3>
+          <div className="input-group">
+            <label htmlFor="number1" className="label">
+              Number1
+            </label>
+            <input
+              type="number"
+              id="number1"
+              name="number1"
+              className="input"
+              placeholder="Enter number 1 value"
+              required
+              onChange={(e) => setNumber1(parseInt(e.target.value))}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="number1" className="label">
+              Number2
+            </label>
+            <input
+              type="number"
+              id="number1"
+              name="number1"
+              className="input"
+              placeholder="Enter number 2 value"
+              required
+              onChange={(e) => setNumber2(parseInt(e.target.value))}
+            />
+          </div>
+          <button className="btn" onClick={addValues}>
+            Submit
+          </button>
+
+          <p className="error">{error}</p>
+          <h5 className="text">Your sum is: {result}</h5>
+        </form>
+      </div>
     </div>
   );
 }
